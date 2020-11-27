9ba82259a30509e5c600c0c85e5da55a90b9f41a
SBen-IV <sinneo@fi.uba.ar>
2020-11-27 15:58:48 -0300

    Map básico

---
+++ common_src/Map.cpp
@@ -0,0 +1,33 @@
+#include "Map.h"
+#include "yaml-cpp/yaml.h"
+
+#define KEY_MAP "map"
+#define KEY_ROW "length"
+#define KEY_COL "width"
+
+Map::Map(const char* file_name) : n_row(0), n_col(0) {
+	YAML::Node file = YAML::LoadFile(file_name);
+
+	n_row = file[KEY_ROW].as<size_t>();
+	n_col = file[KEY_COL].as<size_t>();
+
+	map = new int*[n_row];
+
+	for (std::size_t i = 0; i < n_row; ++i) {
+		map[i] = new int[n_col];
+	}
+
+	for (std::size_t i = 0; i < n_row; ++i) {
+		for (std::size_t j = 0; j < n_col; ++j) {
+			map[i][j] = file[KEY_MAP][i][j].as<int>();
+		}
+	}
+}
+
+Map::~Map() {
+	for (std::size_t i = 0; i < n_row; ++i) {
+		delete[] map[i];
+	}
+
+	delete[] map;
+}
---
+++ common_src/Map.h
@@ -0,0 +1,16 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <cstddef>
+
+class Map {
+private:
+	size_t n_row;
+	size_t n_col;
+	int** map;
+public:
+	Map(const char* file_name);
+	~Map();
+};
+
+#endif
