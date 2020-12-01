2d4615578d49ad3cf46fc53d98d4934138a10f5a
RiedelNicolas <nariedel99@gmail.com>
2020-12-01 14:20:44 -0300

    agrego imagen de fondo

--- ClientLogin/ui_Login.h
+++ ClientLogin/ui_Login.h
@@ -1,13 +1,10 @@
 /********************************************************************************
-** Form generated from reading UI file 'logIn.ui'
+** Form generated from reading UI file ''
 **
 ** Created by: Qt User Interface Compiler version 5.9.7
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
-
-#ifndef UI_LOGIN_H
-#define UI_LOGIN_H
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
@@ -97,9 +94,9 @@ public:
     {
         Login->setWindowTitle(QApplication::translate("Login", "Form", Q_NULLPTR));
         connectButton->setText(QApplication::translate("Login", "Connect", Q_NULLPTR));
-        label->setText(QApplication::translate("Login", "Name", Q_NULLPTR));
-        label_2->setText(QApplication::translate("Login", "Host", Q_NULLPTR));
-        label_3->setText(QApplication::translate("Login", "Port", Q_NULLPTR));
+        label->setText(QApplication::translate("Login", "<html><head/><body><p><span style=\" color:#cc0000;\">Name</span></p></body></html>", Q_NULLPTR));
+        label_2->setText(QApplication::translate("Login", "<html><head/><body><p><span style=\" color:#cc0000;\">Host</span></p></body></html>", Q_NULLPTR));
+        label_3->setText(QApplication::translate("Login", "<html><head/><body><p><span style=\" color:#cc0000;\">Port</span></p></body></html>", Q_NULLPTR));
         label_4->setText(QApplication::translate("Login", "<html><head/><body><p><span style=\" font-family:'-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji'; font-size:xx-large; font-weight:600; color:#cc0000; background-color:#ffffff;\">Wolfenstein</span><span style=\" font-family:'-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji'; font-size:xx-large; font-weight:600; color:#24292e; background-color:#ffffff;\"/></p><p><br/></p></body></html>", Q_NULLPTR));
         label_5->setText(QApplication::translate("Login", "<html><head/><body><p><span style=\" font-size:36pt; color:#4e9a06;\">3D</span></p></body></html>", Q_NULLPTR));
     } // retranslateUi
@@ -112,4 +109,3 @@ namespace Ui {
 
 QT_END_NAMESPACE
 
-#endif // UI_LOGIN_H
