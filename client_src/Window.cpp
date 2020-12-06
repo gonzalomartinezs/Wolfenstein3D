5e803ceb89e1038e77e2db52953a32e3a65f2ab7
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2020-12-06 02:56:56 -0300

    Agrego wrapper de SDL_Window

---
+++ client_src/Window.cpp
@@ -0,0 +1,38 @@
+#include "Window.h"
+
+Window::Window(std::string name, int width, int height, int pos_x, int pos_y,
+               Uint32 flag) : width(width), height(height) {
+    int errCode = SDL_Init(SDL_INIT_VIDEO);
+    if (errCode) {
+        throw ; // exception
+    }
+    this->window = SDL_CreateWindow(name.c_str(), pos_x, pos_y, width,
+                                    height, flag);
+    if (this->window == nullptr) {
+        throw ; // exception
+    }
+    this->renderer = SDL_CreateRenderer(this->window, -1,
+                                        SDL_RENDERER_ACCELERATED);
+    if (this->renderer == nullptr) {
+        throw ; // exception
+    }
+}
+
+void Window::render() {
+    SDL_RenderPresent(this->renderer);
+}
+
+SDL_Renderer *Window::getRenderer() const {
+    return this->renderer;
+}
+
+Window::~Window() {
+    if (this->renderer) {
+        SDL_DestroyRenderer(this->renderer);
+        this->renderer = nullptr;
+    }
+    if (this->window) {
+        SDL_DestroyWindow(this->window);
+        this->window = nullptr;
+    }
+}
