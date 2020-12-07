435b8694e829823f01078d5c054173845f468707
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2020-12-07 18:25:55 -0300

    Agrego PlayerView y reordeno directorios

---
+++ client_src/PlayerView.h
@@ -0,0 +1,23 @@
+#ifndef BOLUDECES_PLAYERVIEW_H
+#define BOLUDECES_PLAYERVIEW_H
+
+class PlayerView {
+private:
+    float camera_plane_x;
+    float camera_plane_y;
+public:
+    // el vector (plane_x, plane_y) debe ser perpendicular al vector direccion
+    // del jugador
+    PlayerView(float plane_x, float plane_y): camera_plane_x(plane_x),
+                                                camera_plane_y(plane_y){}
+
+    void movePlaneX(float movement);
+    void movePlaneY(float movement);
+    float getPlaneX() const;
+    float getPlaneY() const;
+
+    ~PlayerView(){}
+};
+
+
+#endif //BOLUDECES_PLAYERVIEW_H
