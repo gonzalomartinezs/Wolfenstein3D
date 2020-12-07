c69f87bca4f0ae5eac38fb6953ba5f5f1e8581de
ezezbogar <ezezbogar@gmail.com>
2020-12-07 19:08:00 -0300

    Continuo Game y Player

--- server_src/Game.h
+++ server_src/Game.h
@@ -2,19 +2,28 @@
 #define WOLFENSTEIN3D_GAME_H
 
 #include "Player.h"
+#include "ThClient.h"
+#include "../common_src/Timer.h"
 
 class Game {
-private:
+ private:
     std::vector<Player> players;
-
-public:
+    std::vector<ThClient*>& clients;
+    bool isRunning;
+ public:
     /* Constructor */
-    Game();
+    Game(std::vector<ThClient*>& clients);
 
-    void update();
+    void execute();
 
     /* Destructor */
     ~Game();
 
+ private:
+    void getInstructions();
+
+    void update();
+
+    void sendUpdate();
 };
 #endif  // WOLFENSTEIN3D_GAME_H
