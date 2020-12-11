404dbac3adf4d642f23912e675aaf70d23cc40b5
ezezbogar <ezezbogar@gmail.com>
2020-12-11 05:32:21 -0300

    ProtectedQueue a Template

--- server_src/ProtectedQueue.h
+++ server_src/ProtectedQueue.h
@@ -1,19 +1,42 @@
-#ifndef PROTECTEDQUEUE_H
-#define PROTECTEDQUEUE_H
+#ifndef WOLFENSTEIN3D_PROTECTEDQUEUE_H
+#define WOLFENSTEIN3D_PROTECTEDQUEUE_H
 
 #include <queue>
 #include <mutex>
 
+template <class T>
 class ProtectedQueue {
 private:
 	std::mutex m;
-	std::queue<uint8_t> queue;
+	std::queue<T> queue;
 public:
+    /* Constructor */
 	ProtectedQueue();
-	void push(const uint8_t element);
+	void push(const T element);
 	bool isEmpty() const;
-	uint8_t pop();
+	T pop();
+	/* Destructor */
 	~ProtectedQueue();
 };
 
-#endif
+template<class T> ProtectedQueue<T>::ProtectedQueue() {}
+
+template<class T> void ProtectedQueue<T>::push(const T element) {
+    std::lock_guard<std::mutex> lock(m);
+    queue.push(element);
+}
+
+template<class T> bool ProtectedQueue<T>::isEmpty() const {
+    return queue.empty();
+}
+
+template<class T> T ProtectedQueue<T>::pop() {
+    std::lock_guard<std::mutex> lock(m);
+    T element = queue.front();
+    queue.pop();
+    return element;
+}
+
+template<class T> ProtectedQueue<T>::~ProtectedQueue() {}
+
+#endif  // WOLFENSTEIN3D_PROTECTEDQUEUE_H
