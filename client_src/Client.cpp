527a5a831058fd727adacf9f7ad6d1e1e32a8483
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2020-12-15 03:46:09 -0300

    El juego más aburrido del mundo

--- client_src/Client.cpp
+++ client_src/Client.cpp
@@ -51,13 +51,10 @@ void
 Client::_assignPlayerCoordenates(DirectedPositionable &player, PlayerView &view,
                                  std::vector<float> &coordinates,
                                  int player_size, uint8_t *bytes_received) {
-    uint8_t received_uints[FLOAT_SIZE];
-    for (std::size_t i = 0; i < player_size; i++) {
-        received_uints[i%FLOAT_SIZE] = bytes_received[i];
-        if((i%FLOAT_SIZE)==(FLOAT_SIZE-1)){
-            float received_float = *(float*)(received_uints);
-            coordinates.push_back(received_float);
-        }
+    float received;
+    for (std::size_t i = 0; i < PLAYER_ATTRIBUTES; i++) {
+        memcpy(&received, bytes_received + FLOAT_SIZE * i, FLOAT_SIZE);
+        coordinates.push_back(received);
     }
     player.setX(coordinates[0]);
     player.setY(coordinates[1]);
@@ -72,22 +69,30 @@ void Client::_assignOtherPlayersCoordenates(uint8_t *bytes_received,
                                             uint8_t bytes_to_receive,
                                             std::vector<DirectedPositionable> &players,
                                             std::vector<float> &coordinates) {
-    uint8_t received_uints[FLOAT_SIZE];
-    int player_size = PLAYER_ATTRIBUTES * FLOAT_SIZE;
-    int other_players_size = (PLAYERS_ATTRIBUTES - 1) * FLOAT_SIZE + 1;
 
-    for (std::size_t i = player_size; i < bytes_to_receive; i++) {
-        if ((i-player_size)%other_players_size == (other_players_size-1)){ // si es el ultimo byte del mensaje
-            coordinates.push_back((float)bytes_received[i]);
-        } else {
-            received_uints[i%FLOAT_SIZE] = bytes_received[i];
-            if((i%FLOAT_SIZE)==(FLOAT_SIZE-1)){
-                float received_float = *(float*)(received_uints);
-                coordinates.push_back(received_float);
-            }
-        }
+    float received;
+    uint8_t texture;
+    int user_size = PLAYER_ATTRIBUTES * FLOAT_SIZE;
+    int players_size = ((PLAYERS_ATTRIBUTES - 1) * FLOAT_SIZE + 1);
+    int bytes_left = bytes_to_receive - user_size;
+    int players_amount = bytes_left / players_size;
+
+    for (int i = 0; i < players_amount; i++) {
+        memcpy(&received, bytes_received + user_size + i*players_size, FLOAT_SIZE);
+        coordinates.push_back(received);
+        memcpy(&received, bytes_received + user_size + i*players_size + FLOAT_SIZE, FLOAT_SIZE);
+        coordinates.push_back(received);
+        memcpy(&received, bytes_received + user_size + i*players_size + 2*FLOAT_SIZE, FLOAT_SIZE);
+        coordinates.push_back(received);
+        memcpy(&received, bytes_received + user_size + i*players_size + 3*FLOAT_SIZE, FLOAT_SIZE);
+        coordinates.push_back(received);
+        memcpy(&texture, bytes_received + user_size + i*players_size + 4*FLOAT_SIZE, 1);
+        coordinates.push_back((float)texture);
     }
+
     for(std::size_t j=PLAYER_ATTRIBUTES; j < coordinates.size(); j+=PLAYERS_ATTRIBUTES){
+        std::cout << "Player " << j << ", X: " << coordinates[j] << ", Y: " << coordinates[j+1] <<
+        ", dirX: " << coordinates[j+2] <<", dirY: " << coordinates[j+3] << ", tex: " << coordinates[j+4] << std::endl;
         DirectedPositionable other_player(coordinates[j],
                                           coordinates[j + 1],
                                           coordinates[j+2],
@@ -95,6 +100,7 @@ void Client::_assignOtherPlayersCoordenates(uint8_t *bytes_received,
                                           TextureID(coordinates[j+4]));
         players.push_back(other_player);
     }
+    std::cout << std::endl;
 }
 
 
