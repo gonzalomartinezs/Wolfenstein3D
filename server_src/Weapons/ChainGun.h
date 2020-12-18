e7cec7297ce9892812dd7e54f24f136ec689e7ff
ezezbogar <ezezbogar@gmail.com>
2020-12-18 04:55:03 -0300

    Finalizo ChainGun

--- server_src/Weapons/ChainGun.h
+++ server_src/Weapons/ChainGun.h
@@ -2,9 +2,23 @@
 #define WOLFENSTEIN3D_CHAINGUN_H
 
 #include "Weapon.h"
+#include "../../common_src/Timer.h"
 
-class Chaingun : public Weapon {
+class ChainGun : public Weapon {
+private:
+    Timer fireTimer;
+    float lastShotDelay;
 
+public:
+    /* Constructor */
+    ChainGun();
+
+    void startShooting() override;
+
+    void fireTheGun(std::vector<Player> &players, int shootingPlayerNumber, const Map &map) override;
+
+    /* Destructor */
+    ~ChainGun();
 };
 
 #endif //WOLFENSTEIN3D_CHAINGUN_H
