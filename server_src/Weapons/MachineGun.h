5948a8d1c26bbbc69046eb272e0e27a2fe7b6f88
ezezbogar <ezezbogar@gmail.com>
2020-12-21 17:44:01 -0300

    Termino MachineGun

--- server_src/Weapons/MachineGun.h
+++ server_src/Weapons/MachineGun.h
@@ -1,9 +1,28 @@
 #ifndef WOLFENSTEIN_MACHINEGUN_H
 #define WOLFENSTEIN_MACHINEGUN_H
 
-class MachineGun {
+#include "Weapon.h"
+#include "../../common_src/Timer.h"
 
+class MachineGun : public Weapon {
+private:
+    Timer fireTimer;
+    Timer burstTimer;
+    float lastShotDelay;
+    int burstBulletCounter;
+    bool isInBurstRecover;
+    bool isFirstBulletOfTheBurst;
+
+public:
+    /* Constructor */
+    MachineGun();
+
+    void startShooting() override;
+
+    void fireTheGun(std::vector<Player> &players, int shootingPlayerNumber, const Map &map) override;
+
+    /* Destructor */
+    ~MachineGun();
 };
-
 
 #endif  // WOLFENSTEIN_MACHINEGUN_H
