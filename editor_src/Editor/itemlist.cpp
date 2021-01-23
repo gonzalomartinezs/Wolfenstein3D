671baae16c2442e117c38c6f3df009af95b72cbe
RiedelNicolas <nariedel99@gmail.com>
2021-01-23 14:34:28 -0300

    creo la clase itemlist

---
+++ editor_src/Editor/itemlist.cpp
@@ -0,0 +1,6 @@
+#include "itemlist.h"
+
+Itemlist::Itemlist()
+{
+
+}
---
+++ editor_src/Editor/itemlist.h
@@ -0,0 +1,12 @@
+#ifndef ITEMLIST_H
+#define ITEMLIST_H
+
+#include <QListWidget>
+
+class Itemlist : public QListWidget
+{
+public:
+    Itemlist();
+};
+
+#endif // ITEMLIST_H
