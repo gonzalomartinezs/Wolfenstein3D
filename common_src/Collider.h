a45993166bccd2ad74d224d2739f53e98b45b746
SBen-IV <sinneo@fi.uba.ar>
2021-02-01 16:10:23 -0300

    Agregado comentario en Collider.

--- common_src/Collider.h
+++ common_src/Collider.h
@@ -6,7 +6,9 @@ private:
 	float x, y, radius;
 public:
 	Collider(float _x, float _y, float _radius);
+	//Colisión con rectángulo.
 	bool collidesWith(float _x, float _y, float width, float height) const;
+	//Colisión con círculo.
 	bool collidesWith(const Collider& other) const;
 	~Collider();
 };
