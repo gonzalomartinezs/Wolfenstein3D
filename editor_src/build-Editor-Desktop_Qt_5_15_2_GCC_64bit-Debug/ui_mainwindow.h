dcb21d5ad0417797659e380fbbbab1c95499012b
RiedelNicolas <nariedel99@gmail.com>
2021-02-05 20:28:03 -0300

    genero el template con la itemlist

--- editor_src/build-Editor-Desktop_Qt_5_15_2_GCC_64bit-Debug/ui_mainwindow.h
+++ editor_src/build-Editor-Desktop_Qt_5_15_2_GCC_64bit-Debug/ui_mainwindow.h
@@ -11,8 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QListView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +28,19 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QListView *listView;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QWidget *widget;
+    QWidget *formLayoutWidget;
+    QFormLayout *formLayout;
+    QLabel *label_2;
+    QSpinBox *spinBox;
+    QLabel *label;
+    QSpinBox *spinBox_2;
+    QSpinBox *spinBox_3;
+    QLabel *label_3;
+    QPushButton *pushButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,13 +48,66 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 600);
+        MainWindow->resize(363, 327);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        listView = new QListView(centralwidget);
+        listView->setObjectName(QString::fromUtf8("listView"));
+        listView->setGeometry(QRect(0, 0, 121, 131));
+        scrollArea = new QScrollArea(centralwidget);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setGeometry(QRect(120, 0, 241, 281));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 239, 279));
+        scrollArea->setWidget(scrollAreaWidgetContents);
+        widget = new QWidget(centralwidget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(60, 230, 61, 51));
+        formLayoutWidget = new QWidget(centralwidget);
+        formLayoutWidget->setObjectName(QString::fromUtf8("formLayoutWidget"));
+        formLayoutWidget->setGeometry(QRect(0, 130, 121, 101));
+        formLayout = new QFormLayout(formLayoutWidget);
+        formLayout->setObjectName(QString::fromUtf8("formLayout"));
+        formLayout->setContentsMargins(0, 0, 0, 0);
+        label_2 = new QLabel(formLayoutWidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        formLayout->setWidget(1, QFormLayout::LabelRole, label_2);
+
+        spinBox = new QSpinBox(formLayoutWidget);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+
+        formLayout->setWidget(1, QFormLayout::FieldRole, spinBox);
+
+        label = new QLabel(formLayoutWidget);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        formLayout->setWidget(2, QFormLayout::LabelRole, label);
+
+        spinBox_2 = new QSpinBox(formLayoutWidget);
+        spinBox_2->setObjectName(QString::fromUtf8("spinBox_2"));
+
+        formLayout->setWidget(3, QFormLayout::FieldRole, spinBox_2);
+
+        spinBox_3 = new QSpinBox(formLayoutWidget);
+        spinBox_3->setObjectName(QString::fromUtf8("spinBox_3"));
+
+        formLayout->setWidget(2, QFormLayout::FieldRole, spinBox_3);
+
+        label_3 = new QLabel(formLayoutWidget);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        formLayout->setWidget(3, QFormLayout::LabelRole, label_3);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(0, 230, 61, 51));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 23));
+        menubar->setGeometry(QRect(0, 0, 363, 23));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -49,6 +121,10 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Ancho", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Altura", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "<html><head/><body><p><span style=\" font-size:9pt;\">Jugadores</span></p></body></html>", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Aplicar", nullptr));
     } // retranslateUi
 
 };
