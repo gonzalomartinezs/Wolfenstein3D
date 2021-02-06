bcc78f8da5d7e0f5f9c3d0f59a4729ae3493e048
ezezbogar <ezezbogar@gmail.com>
2021-02-06 17:10:23 -0300

    Ahora Bot hereda de Player

--- server_src/Bots/Bot.h
+++ server_src/Bots/Bot.h
@@ -1,30 +1,32 @@
 #ifndef WOLFENSTEIN3D_BOT_H
 #define WOLFENSTEIN3D_BOT_H
 
-#include "../../common_src/DirectedPositionable.h"
 #include "../../common_src/Map.h"
 #include "../Player.h"
 #include "lua.hpp"
 
-class Bot : public DirectedPositionable {
+class Bot : public Player {
 private:
     lua_State* L;
 
 public:
     /* Constructor */
-    Bot(float moveSpeed, float rotSpeed, float posX, float posY);
+    Bot(const Configuration& config_stats,
+        const Configuration& config_map,
+        const uint8_t _player_number);
 
-    void update(std::vector<Player> &players, int botNumber, const Map &map);
+    void getState(std::vector<Player*> &players, int botNumber, const Map &map) override;
 
     /* Destructor */
     ~Bot();
 
+private:
+
     void _pushMap(const Map& map);
 
-    void _pushPlayersPositions(std::vector<Player> &players, int botNumber);
+    void _pushPlayersPositions(std::vector<Player*> &players, int botNumber);
 
     void _pushBotInfo();
-
 };
 
 #endif //WOLFENSTEIN3D_BOT_H
