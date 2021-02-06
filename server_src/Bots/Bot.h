ea7f504f304257db9b404f41d1e631f60055aecc
ezezbogar <ezezbogar@gmail.com>
2021-02-06 18:18:45 -0300

    Documento

--- server_src/Bots/Bot.h
+++ server_src/Bots/Bot.h
@@ -15,17 +15,20 @@ public:
         const Configuration& config_map,
         const uint8_t _player_number);
 
+    /* Actualiza el estado del Bot segun lo indique el script de Lua */
     void getState(std::vector<Player*> &players, int botNumber, const Map &map) override;
 
     /* Destructor */
     ~Bot();
 
 private:
-
+    /* Pushea el mapa al stack de Lua */
     void _pushMap(const Map& map);
 
+    /* Pushea las posiciones del resto de los jugadores al stack de Lua */
     void _pushPlayersPositions(std::vector<Player*> &players, int botNumber);
 
+    /* Pushea la informacion de la posicion y direccion del Bot en cuestion al stack de Lua */
     void _pushBotInfo();
 };
 
