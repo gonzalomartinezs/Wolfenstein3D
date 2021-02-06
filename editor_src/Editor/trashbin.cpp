a2bc078ab942cc0f661c25e921c1dddc1ac1f08f
RiedelNicolas <nariedel99@gmail.com>
2021-02-06 02:33:08 -0300

    implemento trashbin

--- editor_src/Editor/trashbin.cpp
+++ editor_src/Editor/trashbin.cpp
@@ -1,6 +1,22 @@
 #include "trashbin.h"
 
+#include "mainwindow.h"
+#include "itemList.h"
+#include <QDrag>
+#include <QDragEnterEvent>
+#include <QMimeData>
+#include <QPainter>
+
 TrashBin::TrashBin(QWidget *parent) : QWidget(parent)
 {
 
+}
+
+
+void TrashBin::dragEnterEvent(QDragEnterEvent *event)
+{
+    if (event->mimeData()->hasFormat(ItemList::editorMimeType()))
+        event->accept();
+    else
+        event->ignore();
 }
--- editor_src/Editor/trashbin.h
+++ editor_src/Editor/trashbin.h
@@ -2,6 +2,7 @@
 #define TRASHBIN_H
 
 #include <QWidget>
+#include  <QVector>
 
 class TrashBin : public QWidget
 {
@@ -11,6 +12,15 @@ public:
 
 signals:
 
+protected:
+    void dragEnterEvent(QDragEnterEvent *event) override;
+    /*
+    void dragLeaveEvent(QDragLeaveEvent *event) override;
+    void dragMoveEvent(QDragMoveEvent *event) override;
+    void dropEvent(QDropEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void paintEvent(QPaintEvent *event) override;
+    */
 };
 
 #endif // TRASHBIN_H
