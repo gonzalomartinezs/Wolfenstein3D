030a57d83fc8222f98dfd782ec56ef2bae7a0b85
ezezbogar <ezezbogar@gmail.com>
2021-02-08 05:10:37 -0300

    Creador de LeaderBoard (Sin probar)

---
+++ server_src/LeaderBoard.h
@@ -0,0 +1,24 @@
+#ifndef WOLFENSTEIN3D_LEADERBOARD_H
+#define WOLFENSTEIN3D_LEADERBOARD_H
+
+#define MAX_LEADERBOARD 5
+
+#include "Player.h"
+
+class LeaderBoard {
+public:
+    /* Constructor */
+    LeaderBoard();
+
+    int loadLeaderBoard(uint8_t* msg, std::vector<Player*>& players);
+
+    /* Destructor */
+    ~LeaderBoard();
+
+private:
+    int _loadKills(uint8_t* msg, std::vector<Player*>& players);
+    int _loadScore(uint8_t* msg, std::vector<Player*>& players);
+    int _loadBullets(uint8_t* msg, std::vector<Player*>& players);
+};
+
+#endif //WOLFENSTEIN3D_LEADERBOARD_H
