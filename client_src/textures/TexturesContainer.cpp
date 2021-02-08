a22301c745c130c652c79ac42079680ee8f5e82d
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2021-02-08 12:16:39 -0300

    Actualizo paths de texturas

--- client_src/textures/TexturesContainer.cpp
+++ client_src/textures/TexturesContainer.cpp
@@ -5,6 +5,7 @@
 #define DOOR_PATH "../client_src/textures/img/sprites/door.bmp"
 #define INVERTED_DOOR_PATH "../client_src/textures/img/sprites/inv_door.bmp"
 #define BARREL_PATH "../client_src/textures/img/sprites/barrel.bmp"
+
 #define GUARD_0_PATH "../client_src/textures/img/sprites/guard_0.bmp"
 #define GUARD_1_PATH "../client_src/textures/img/sprites/guard_1.bmp"
 #define GUARD_2_PATH "../client_src/textures/img/sprites/guard_2.bmp"
@@ -30,21 +31,36 @@
 #define NOT_KEY_PATH "../client_src/textures/img/miscellaneous/has_not_key.bmp"
 #define BACKGROUND "../client_src/textures/img/miscellaneous/back.png"
 #define PL_HUD "../client_src/textures/img/miscellaneous/HUD_1.png"
-#define PISTOL_PL "../client_src/textures/img/sprites/magnum.bmp"
-#define PISTOL_PL_1 "../client_src/textures/img/sprites/magnum(1).bmp"
-#define PISTOL_PL_2 "../client_src/textures/img/sprites/magnum(2).bmp"
-#define PISTOL_PL_3 "../client_src/textures/img/sprites/magnum(3).bmp"
-#define PISTOL_PL_4 "../client_src/textures/img/sprites/magnum(4).bmp"
-#define PISTOL_PL_5 "../client_src/textures/img/sprites/magnum(5).bmp"
-#define PISTOL_PL_6 "../client_src/textures/img/sprites/magnum(6).bmp"
-#define PISTOL_PL_7 "../client_src/textures/img/sprites/magnum(7).bmp"
-#define PISTOL_PL_8 "../client_src/textures/img/sprites/magnum(8).bmp"
-#define PISTOL_PL_9 "../client_src/textures/img/sprites/magnum(9).bmp"
-#define PISTOL_PL_10 "../client_src/textures/img/sprites/magnum(10).bmp"
-#define PISTOL_PL_11 "../client_src/textures/img/sprites/magnum(11).bmp"
-#define PISTOL_PL_12 "../client_src/textures/img/sprites/magnum(12).bmp"
-#define PISTOL_PL_13 "../client_src/textures/img/sprites/magnum(13).bmp"
-#define PISTOL_PL_14 "../client_src/textures/img/sprites/magnum(14).bmp"
+
+#define KNIFE_PL "../client_src/textures/img/sprites/knife_0.bmp"
+#define KNIFE_PL_1 "../client_src/textures/img/sprites/knife_1.bmp"
+#define KNIFE_PL_2 "../client_src/textures/img/sprites/knife_2.bmp"
+#define KNIFE_PL_3 "../client_src/textures/img/sprites/knife_3.bmp"
+#define PISTOL_PL "../client_src/textures/img/sprites/magnum_0.bmp"
+#define PISTOL_PL_1 "../client_src/textures/img/sprites/magnum_1.bmp"
+#define PISTOL_PL_2 "../client_src/textures/img/sprites/magnum_2.bmp"
+#define PISTOL_PL_3 "../client_src/textures/img/sprites/magnum_3.bmp"
+#define PISTOL_PL_4 "../client_src/textures/img/sprites/magnum_4.bmp"
+#define PISTOL_PL_5 "../client_src/textures/img/sprites/magnum_5.bmp"
+#define PISTOL_PL_6 "../client_src/textures/img/sprites/magnum_6.bmp"
+#define PISTOL_PL_7 "../client_src/textures/img/sprites/magnum_7.bmp"
+#define PISTOL_PL_8 "../client_src/textures/img/sprites/magnum_8.bmp"
+#define PISTOL_PL_9 "../client_src/textures/img/sprites/magnum_9.bmp"
+#define PISTOL_PL_10 "../client_src/textures/img/sprites/magnum_10.bmp"
+#define PISTOL_PL_11 "../client_src/textures/img/sprites/magnum_11.bmp"
+#define PISTOL_PL_12 "../client_src/textures/img/sprites/magnum_12.bmp"
+#define PISTOL_PL_13 "../client_src/textures/img/sprites/magnum_13.bmp"
+#define PISTOL_PL_14 "../client_src/textures/img/sprites/magnum_14.bmp"
+#define MACHINEGUN_PL "../client_src/textures/img/sprites/machinegun_0.bmp"
+#define MACHINEGUN_PL_1 "../client_src/textures/img/sprites/machinegun_1.bmp"
+#define MACHINEGUN_PL_2 "../client_src/textures/img/sprites/machinegun_2.bmp"
+#define CHAINGUN_PL "../client_src/textures/img/sprites/chaingun_0.bmp"
+#define CHAINGUN_PL_1 "../client_src/textures/img/sprites/chaingun_1.bmp"
+#define CHAINGUN_PL_2 "../client_src/textures/img/sprites/chaingun_2.bmp"
+#define RPG_PL "../client_src/textures/img/sprites/rpg_0.bmp"
+#define RPG_PL_1 "../client_src/textures/img/sprites/rpg_1.bmp"
+#define RPG_PL_2 "../client_src/textures/img/sprites/rpg_2.bmp"
+#define RPG_PL_3 "../client_src/textures/img/sprites/rpg_3.bmp"
 
 
 TexturesContainer::TexturesContainer(SDL_Renderer *renderer,
@@ -102,15 +118,26 @@ void TexturesContainer::_loadStaticTextures() {
     static_textures.emplace(RPG_HUD, new Texture(RPGHUD, this->renderer));
     static_textures.emplace(HasKey, new Texture(KEY_PATH, this->renderer));
     static_textures.emplace(HasNotKey, new Texture(NOT_KEY_PATH, this->renderer));
-    static_textures.emplace(Background,new Texture(BACKGROUND,
-                                                   this->renderer, this->window_surface));
+    static_textures.emplace(Background,new Texture(BACKGROUND, this->renderer, this->window_surface));
     static_textures.emplace(HUD, new Texture(PL_HUD, this->renderer, this->window_surface));
 }
 
 void TexturesContainer::_loadDynamicTextures() {
+    dynamic_textures.emplace(Knife_Pl,new DynamicTexture(
+            std::vector<std::string>{KNIFE_PL, KNIFE_PL_1, KNIFE_PL_2, KNIFE_PL_3},
+            this->renderer, this->window_surface, 400));
     dynamic_textures.emplace(Pistol_Pl,new DynamicTexture(
             std::vector<std::string>{PISTOL_PL, PISTOL_PL_1, PISTOL_PL_2, PISTOL_PL_3, PISTOL_PL_4,
                                      PISTOL_PL_5, PISTOL_PL_6, PISTOL_PL_7, PISTOL_PL_8, PISTOL_PL_9,
                                      PISTOL_PL_10, PISTOL_PL_11, PISTOL_PL_12, PISTOL_PL_13, PISTOL_PL_14},
-                                     this->renderer, this->window_surface, 1000));
+                                     this->renderer, this->window_surface, 500));
+    dynamic_textures.emplace(MachineGun_Pl,new DynamicTexture(
+            std::vector<std::string>{MACHINEGUN_PL, MACHINEGUN_PL_1, MACHINEGUN_PL_2},
+            this->renderer, this->window_surface, 100));
+    dynamic_textures.emplace(ChainGun_Pl,new DynamicTexture(
+            std::vector<std::string>{CHAINGUN_PL, CHAINGUN_PL_1, CHAINGUN_PL_2},
+            this->renderer, this->window_surface, 250));
+    dynamic_textures.emplace(RPG_Pl,new DynamicTexture(
+            std::vector<std::string>{RPG_PL, RPG_PL_1, RPG_PL_2, RPG_PL_3},
+            this->renderer, this->window_surface, 250));
 }
