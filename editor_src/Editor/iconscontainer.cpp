bc140214dcc00a0eb0e3ae47b3c112a26282e0e5
RiedelNicolas <nariedel99@gmail.com>
2021-02-13 22:24:13 -0300

    ahora anda con cmake

--- editor_src/Editor/iconscontainer.cpp
+++ editor_src/Editor/iconscontainer.cpp
@@ -1,11 +1,27 @@
 #include "iconscontainer.h"
 #include <QResource>
 
+
+#define PUERTA "/icons/arc-triomphe.png"
+#define CUCHILLO "icons/bowie-knife.png"
+
+/*
+<file alias="2">icons/brick-wall.png</file>
+<file alias="3">icons/flying-shuriken.png</file>
+<file alias="4">icons/heart-tower.png</file>
+<file alias="5">icons/luger.png</file>
+<file alias="6">icons/player-base.png</file>
+<file alias="7">icons/revolver.png</file>
+<file alias="8">icons/steyr-aug.png</file>
+<file alias="9">icons/stick-grenade.png</file>
+<file alias="10">icons/tesla-turret.png</file>
+<file alias="11">icons/uzi.png</file>
+*/
+
 IconsContainer::IconsContainer(unsigned in_size) {
-    //Q_INIT_RESOURCE(editor);
     QResource::registerResource("editor.rcc");
     this->size = in_size;
-
+    /*
     unsigned i = 0;
     bool done = false;
     while( !done ) {
@@ -17,6 +33,10 @@ IconsContainer::IconsContainer(unsigned in_size) {
            done = true;
        }
     }
+    */
+    QPixmap newImage;
+     newImage.load( PUERTA );
+    this->icons.push_back(newImage.scaled(this->size, this->size));
 }
 
 const QPixmap& IconsContainer::getIcon(int id)const {
