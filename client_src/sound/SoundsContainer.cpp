e594b09d8b0751f0d86a755d0534235373655c5d
gonzalomartinezs <gonzalomartinezsastre@gmail.com>
2021-02-17 02:43:55 -0300

    container sin memoria dinamica

--- client_src/sound/SoundsContainer.cpp
+++ client_src/sound/SoundsContainer.cpp
@@ -1,35 +1,27 @@
 #include "SoundsContainer.h"
 
-#define BACK_MUSIC "resources/music.wav"
+#define BACK_MUSIC "../client_src/sound/resources/music.wav"
+#define PISTOL_SFX "../client_src/sound/resources/high.wav"
 
 SoundsContainer::SoundsContainer() {
     _loadMusic();
     _loadSFX();
 }
 
-Music *SoundsContainer::getMusic(SoundID id) {
+Music& SoundsContainer::getMusic(SoundID id) {
     return music.at(id);
 }
 
-SoundEffect *SoundsContainer::getSFX(SoundID id) {
+SoundEffect& SoundsContainer::getSFX(SoundID id) {
     return sfx.at(id);
 }
 
-SoundsContainer::~SoundsContainer() {
-    for(auto& _music: music){
-        delete _music.second;
-        _music.second = nullptr;
-    }
-    for(auto& _sfx: sfx){
-        delete _sfx.second;
-        _sfx.second = nullptr;
-    }
-}
+SoundsContainer::~SoundsContainer() {}
 
 void SoundsContainer::_loadMusic() {
-    this->music.emplace(BackMusic, new Music(BACK_MUSIC));
+    this->music.emplace(BackMusic, BACK_MUSIC);
 }
 
 void SoundsContainer::_loadSFX() {
-
+    this->sfx.emplace(PistolSFX, PISTOL_SFX);
 }
--- client_src/sound/SoundsContainer.h
+++ client_src/sound/SoundsContainer.h
@@ -7,18 +7,18 @@
 
 class SoundsContainer {
 private:
-    std::unordered_map<SoundID, Music*> music;
-    std::unordered_map<SoundID, SoundEffect*> sfx;
+    std::unordered_map<SoundID, Music> music;
+    std::unordered_map<SoundID, SoundEffect> sfx;
     
 public:
     // Crea un contenedor de sonidos con todos los sonidos del juego cargados.
     SoundsContainer();
 
     // Retorna un puntero a la cancion estatica solicitada.
-    Music* getMusic(SoundID id);
+    Music& getMusic(SoundID id);
 
     // Retorna un puntero al sfx solicitado.
-    SoundEffect* getSFX(SoundID id);
+    SoundEffect& getSFX(SoundID id);
 
     // Libera los recursos utilizados por el contenedor.
     ~SoundsContainer();
