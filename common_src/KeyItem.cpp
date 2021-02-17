385339c2b01150b472bbe46c13a2b09523a858fe
SBen-IV <sinneo@fi.uba.ar>
2021-02-17 12:53:26 -0300

    Cambios minimos

--- common_src/KeyItem.cpp
+++ common_src/KeyItem.cpp
@@ -1,9 +1,11 @@
 #include "KeyItem.h"
 
+#define VALUE 0
+
 KeyItem::KeyItem(const Configuration& config, float _x, float _y) : 
 				Item(_x, _y,
 					static_cast<TextureID>(config.getInt(KEY_TEXTURE)),
-					config.getInt(KEY_VALUE),
+					VALUE,
 					config.getFloat(KEY_RADIUS)) {}
 
 void KeyItem::equipTo(PlayerActions& action) {
