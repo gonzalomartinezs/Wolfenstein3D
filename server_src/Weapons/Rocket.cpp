90de9eb14bb98f30286a1c1357d1e8d34811ed59
ezezbogar <ezezbogar@gmail.com>
2021-02-21 05:50:04 -0300

    En principio termino Rocket

--- server_src/Weapons/Rocket.cpp
+++ server_src/Weapons/Rocket.cpp
@@ -1,9 +1,84 @@
 #include "Rocket.h"
 #include "../Player.h"
 
-Rocket::Rocket(float _x, float _y, float _dir_x, float _dir_y) :
-				DirectedPositionable(_x, _y, _dir_x, _dir_y, None) {}
-//CAMBIAR LA TEXTURA A LA QUE CORRESPONDA
+#define ROCKET_SIZE 0.2 /* Leer del YAML ? */
+#define WALL_SIZE 1
+#define WALKABLE 0
+#define MAX_DAMAGE 100 /* Leer del YAML ? */
+#define MAX_DAMAGE_DISTANCE 3 /* Leer del YAML ? */
 
+Rocket::Rocket(float pos_x, float pos_y, float dir_x, float dir_y, int shootingPlayerNumber) :
+                DirectedPositionable(pos_x, pos_y, dir_x, dir_y, Missile_0) {
+    this->moveSpeed = 0.02; /* Leer del YAML */
+    this->shootingPlayerNumber = shootingPlayerNumber;
+    this->exploded = false;
+}
+
+void Rocket::update(std::vector<Player*>& players, const Map& map) {
+    this->x += this->dir_x * this->moveSpeed;
+    this->y += this->dir_y * this->moveSpeed;
+    if (_collided(players, map)) {
+        _explode(players, map);
+        this->exploded = true;
+    }
+}
+
+void Rocket::_explode(std::vector<Player*>& players, const Map& map) {
+    for (size_t i = 0; i < players.size(); ++i) {
+        float distance = this->distanceTo(*players[i]);
+        uint8_t damage = _calculateDamage(distance);
+        if (distance >= MAX_DAMAGE_DISTANCE) {
+            players[i]->receiveShot(damage);
+            if (players[i]->isDead()) {
+                players[this->shootingPlayerNumber]->increaseKillCounter();
+            }
+        }
+    }
+}
+
+bool Rocket::_collided(std::vector<Player*>& players, const Map& map) {
+    Collider collider(this->x, this->y, ROCKET_SIZE);
+    bool collided = (this->_lookForWallCollision(map, collider) || this->_lookForPlayerCollision(players, collider));
+    return collided;
+}
+
+bool Rocket::_lookForWallCollision(const Map& map, const Collider& collider) {
+    bool collided = false;
+    for (int i = this->x-1; i <= this->x+1; ++i) {
+        for (int j = this->y-1; j <= this->y+1; ++j) {
+            if (map.get(i, j) != WALKABLE) {
+                if (collider.collidesWith(i, j, WALL_SIZE, WALL_SIZE)) {
+                    collided = true;
+                }
+            }
+        }
+    }
+    return collided;
+}
+
+bool Rocket::_lookForPlayerCollision(std::vector<Player*>& players, const Collider& collider) {
+    bool collided = false;
+    for (size_t i = 0; i < players.size(); ++i) {
+        if (static_cast<int>(i) != this->shootingPlayerNumber) {
+            if (collider.collidesWith(players[i]->getCollider()) == true) {
+                collided = true;
+            }
+        }
+    }
+    return collided;
+}
+
+uint8_t Rocket::_calculateDamage(float distance) {
+    float damageDealt = (-1/MAX_DAMAGE_DISTANCE * distance) + 1;
+    if (damageDealt < 0) {
+        damageDealt = 0;
+    }
+    damageDealt = damageDealt * MAX_DAMAGE;
+    return static_cast<uint8_t>(damageDealt);
+}
+
+bool Rocket::hasExploded() {
+    return this->exploded;
+}
 
 Rocket::~Rocket() {}
--- server_src/Weapons/Rocket.h
+++ server_src/Weapons/Rocket.h
@@ -1,15 +1,37 @@
-#ifndef ROCKET_H
-#define ROCKET_H
+#ifndef WOLFENSTEIN3D_ROCKET_H_
+#define WOLFENSTEIN3D_ROCKET_H_
 
+#include <vector>
 #include "../../common_src/DirectedPositionable.h"
+#include "../../common_src/Map.h"
+#include "../../common_src/Collider.h"
 
 class Player;
 
+
 class Rocket : public DirectedPositionable {
+private:
+    float moveSpeed;
+    int shootingPlayerNumber;
+    bool exploded;
+
 public:
-	Rocket(float _x, float _y, float _dir_x, float _dir_y);
-//	void update(std::vector<Player*> players);
-	~Rocket();	
+    /* Constructor */
+    Rocket(float pos_x, float pos_y, float dir_x, float dir_y, int shootingPlayerNumber);
+
+    void update(std::vector<Player*>& players, const Map& map);
+
+    bool hasExploded();
+
+    /* Destructor */
+    ~Rocket();
+
+private:
+    void _explode(std::vector<Player*>& players, const Map& map);
+    uint8_t _calculateDamage(float distance);
+    bool _collided(std::vector<Player*>& players, const Map& map);
+    bool _lookForPlayerCollision(std::vector<Player*>& players, const Collider& collider);
+    bool _lookForWallCollision(const Map& map, const Collider& collider);
 };
 
-#endif
+#endif  // WOLFENSTEIN3D_ROCKET_H_
