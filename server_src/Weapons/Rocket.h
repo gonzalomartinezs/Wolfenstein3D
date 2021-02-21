ae42fd62d476848bad9126cbddef92e2c08eccb8
ezezbogar <ezezbogar@gmail.com>
2021-02-21 18:28:24 -0300

    Documento Rocket

--- server_src/Weapons/Rocket.h
+++ server_src/Weapons/Rocket.h
@@ -20,20 +20,35 @@ public:
         float _max_damage_distance, float pos_x, float pos_y,
         float dir_x, float dir_y, int shootingPlayerNumber);
 
+    /* Actualiza el estado del Rocket (Step-Time) */
     void update(std::vector<Player*>& players, const Map& map);
 
+    /* Devuelve true si el Rocket explotó */
     bool hasExploded() const;
 
+    /* Escribe los primeros 16 bytes del buffer con la informacion de
+     * la posicion del jugador de la siguiente manera (floats):
+     * [ posX, posY, dirX, dirY ] */
     void getPositionData(uint8_t *msg);
 
     /* Destructor */
     ~Rocket();
 
 private:
+    /* Inflige el daño correspondiente a cada uno de los jugadores cercanos */
     void _explode(std::vector<Player*>& players, const Map& map);
+
+    /* Calcula el daño en base a la distancia */
     uint8_t _calculateDamage(float distance);
+
+    /* Devuelve true si detecta algún tipo de colision */
     bool _collided(std::vector<Player*>& players, const Map& map);
+
+    /* Devuelve true si el Rocket esta en colisión con algún jugador que no
+     * sea el que creo el Rocket */
     bool _lookForPlayerCollision(std::vector<Player*>& players, const Collider& collider);
+
+    /* Devuelve true si el Rocket esta en colision con alguna pared */
     bool _lookForWallCollision(const Map& map, const Collider& collider);
 };
 
